11acc33e12aa22810264a410c8ece8af1b47f86d
maxencedemaeyer-creator <maxence.de.maeyer@gmail.com>
2026-09-09 15:21:12 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,35 @@
+import { Routes, Route } from 'react-router-dom'
+import { usePin } from './context/PinContext.jsx'
+import PinLock from './components/PinLock.jsx'
+import AppShell from './components/layout/AppShell.jsx'
+import AccueilView from './views/AccueilView.jsx'
+import CoursParticuliersView from './views/CoursParticuliersView.jsx'
+import VeloView from './views/VeloView.jsx'
+import EtudesView from './views/EtudesView.jsx'
 
+export default function App() {
+  const { unlocked, authReady } = usePin()
+
+  if (!unlocked) {
+    return <PinLock />
+  }
+
+  if (!authReady) {
+    return (
+      <div className="min-h-screen flex items-center justify-center text-slate-400 text-sm">
+        Connexion à la base de données…
+      </div>
+    )
+  }
+
+  return (
+    <Routes>
+      <Route element={<AppShell />}>
+        <Route path="/" element={<AccueilView />} />
+        <Route path="/cours" element={<CoursParticuliersView />} />
+        <Route path="/velo" element={<VeloView />} />
+        <Route path="/etudes" element={<EtudesView />} />
+      </Route>
+    </Routes>
+  )
+}
