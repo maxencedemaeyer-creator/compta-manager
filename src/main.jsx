b277e2b1f5056c42921456df05371167f8a1c287
maxencedemaeyer-creator <maxence.de.maeyer@gmail.com>
2026-09-09 16:17:10 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { PinProvider } from './context/PinContext.jsx'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <PinProvider>
-        <App />
-      </PinProvider>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <PinProvider>
+          <App />
+        </PinProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 )
